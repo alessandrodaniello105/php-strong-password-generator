473c9787803b3d10bfb39c489e9c96228d6b616a
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-27 14:10:12 +0200

    [add]: head.php partials+style.css; [test]: vars

---
+++ data/vars.php
@@ -0,0 +1,5 @@
+<?php
+// Dichiaro una variabile come una stringa con tutti i caratteri possibili, con cui generero la password
+$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPWRSTUVWXYZèé[{+*}}òç@à°#ù§,;.:-_\|<>ì^!"£$%&/()=';
+
+$stylePath = 'style.css';
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/data/vars.php';
+
+include __DIR__ . '/partials/head.php';
+
+?>
+
+
+<body>
+  <div class="container">
+    <h2>Hello World!</h2>
+  </div>
+</body>
+</html>
