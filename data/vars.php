8064062f55b4c0f05f2bc3b050eb9305e280d26f
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-28 20:45:24 +0200

    [edit]: error_msg is now message

--- data/vars.php
+++ data/vars.php
@@ -4,7 +4,7 @@
 $magicStr = htmlentities('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPWRSTUVWXYZèé[{+*}}òç@à°#ù§,;.:-_\|ì^!£$%&/()=');
 
 // $limitLength = (isset($_POST['limit'])) ? $_POST['limit'] : 'unset' ;
-$error_msg = '';
+$message = '';
 
 
 
